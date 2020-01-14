8fea0a1aa718c7c73162dcfa90fd7530b1ba30cd
ncaudill27 <nelson.a.caudill@gmail.com>
2020-01-14 16:00:16 -0500

    Build /login form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,4 +3,8 @@ class SessionsController < ApplicationController
     get "/login" do        
         erb :"sessions/login"
     end
+
+    post "/login" do
+        "#{params[:user]}"
+    end
 end
