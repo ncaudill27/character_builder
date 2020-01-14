d8f269da7428b19182a9b3fe19cef84490ee5633
ncaudill27 <nelson.a.caudill@gmail.com>
2020-01-14 12:30:20 -0500

    Seed Users table

---
+++ db/seeds.rb
@@ -0,0 +1,3 @@
+User.create(username: "Dungeon Crawler", email: "example@example.com", password: "password")
+User.create(username: "Edge Lord", email: "edge@obscure.com", password: "testytest")
+User.create(username: "El Capitan", email: "cpt@example.com", password: "1234567890")
