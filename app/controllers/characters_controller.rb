358988a1dbcaff3a59e2d86be942ceabb539dbdf
ncaudill27 <nelson.a.caudill@gmail.com>
2020-01-15 16:32:40 -0500

    Finish basic Character routes

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -27,7 +27,22 @@ class CharactersController < ApplicationController
 
     get "/characters/:id/edit" do
         @character = Character.find_by(id: params[:id])
-        
+
         erb :"/characters/edit"
+    end
+
+    patch "/characters/:id" do
+        character = Character.find_by(id: params[:id])
+        character.update(params[:character])
+        
+        redirect "/characters/#{character.id}"
+    end
+
+    delete "/characters/:id" do
+        character = Character.find_by(id: params[:id])
+        user = character.user
+        character.destroy
+
+        redirect "/#{user.username}"
     end
 end
