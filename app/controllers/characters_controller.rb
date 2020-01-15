8e40c71ab3ff05425d84b746af76c461d469bcb0
ncaudill27 <nelson.a.caudill@gmail.com>
2020-01-14 21:59:13 -0500

    Give Character #stats hash to complete show.erb

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -4,4 +4,10 @@ class CharactersController < ApplicationController
         @characters = Character.all
         erb :"/characters/index"
     end
+
+    get "/characters/:id" do
+        @character = Character.find_by(id: params[:id])
+
+       erb :"/characters/show" 
+    end
 end
--- app/models/character.rb
+++ app/models/character.rb
@@ -4,5 +4,17 @@ class Character < ActiveRecord::Base
     def owner
         User.find(self.user_id).username
     end
+
+    def stats
+        {
+            "Health Points" => self.hp,
+            "Strength" => self.str,
+            "Dexterity" => self.dex,
+            "Constitution" => self.con,
+            "Intelligence" => self.int,
+            "Wisdom" => self.wis,
+            "Charisma" => self.cha
+        }
+    end
 end
 #? Character.new.errors[:name].any? # => false (MAY NEED)
