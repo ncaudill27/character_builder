6c67b588752403c98082f11d51c652d47349b04c
ncaudill27 <nelson.a.caudill@gmail.com>
2020-01-17 12:44:31 -0500

    Scrape info for Class names and info urls

--- temp.rb
+++ temp.rb
@@ -4,18 +4,29 @@ require 'pry'
 
 doc = Nokogiri::HTML(open("http://dnd5eapi.co/api/classes/"))
 
-binding.pry
-
 # class names
 # doc.text.scan(/name...[a-z]+/i)
 # string names
-# doc.text.scan(/name...[a-z]+/i)[0].match(/[A-Z].+/).to_s
+names = doc.text.scan(/name...[a-z]+/i).collect{|klass| klass.match(/[A-Z].+/).to_s}
 # class urls
-# doc.text.scan(/api\/classes\/[a-z]+/i)
+urls = doc.text.scan(/api\/classes\/[a-z]+/i)
 
 class Klass
   @@all = []
   attr_accessor :url, :name
-  
+  def initialize
+    @@all << self
+  end
+  def self.all
+    @@all
+  end
 end
 
+names.each.with_index do |name, index|
+  c = Klass.new
+  c.name = name
+  c.url = urls[index]
+
+end
+
+binding.pry
