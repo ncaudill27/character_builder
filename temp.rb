3c6250c273ca49e198e5bd77608fe27be88b233b
ncaudill27 <nelson.a.caudill@gmail.com>
2020-01-17 13:22:05 -0500

    Scrape skills from class pages

--- temp.rb
+++ temp.rb
@@ -2,31 +2,43 @@ require 'nokogiri'
 require 'open-uri'
 require 'pry'
 
-doc = Nokogiri::HTML(open("http://dnd5eapi.co/api/classes/"))
+doc = Nokogiri::HTML(open("http://dnd5eapi.co/api/classes/barbarian"))
 
+# Source URL: http://dnd5eapi.co/api/classes/
 # class names
 # doc.text.scan(/name...[a-z]+/i)
 # string names
-names = doc.text.scan(/name...[a-z]+/i).collect{|klass| klass.match(/[A-Z].+/).to_s}
+# names = doc.text.scan(/name...[a-z]+/i).collect{|klass| klass.match(/[A-Z].+/).to_s}
 # class urls
-urls = doc.text.scan(/api\/classes\/[a-z]+/i)
+# urls = doc.text.scan(/\/api\/classes\/[a-z]+/i)
 
-class Klass
-  @@all = []
-  attr_accessor :url, :name
-  def initialize
-    @@all << self
-  end
-  def self.all
-    @@all
-  end
-end
+# class Klass
+#   @@all = []
+#   attr_accessor :url, :name
+#   def initialize
+#     @@all << self
+#   end
+#   def self.all
+#     @@all
+#   end
+# end
 
-names.each.with_index do |name, index|
-  c = Klass.new
-  c.name = name
-  c.url = urls[index]
+# names.each.with_index do |name, index|
+#   c = Klass.new
+#   c.name = name
+#   c.url = urls[index]
 
-end
+# end
 
 binding.pry
+
+# Source Klass.url slugs:
+
+# Hit Die
+# doc.text.match(/hit_die..\d+/).to_s.match(/\d+/).to_s.to_i
+
+
+# doc.text.scan(/\/api\/proficiencies\/[a-z]*-?[a-z]*/i)
+
+# Skills
+# doc.text.scan(/\/api\/proficiencies\/[a-z]*-?[a-z]*/i).collect{|i| i.split("proficiencies/")[1]}
