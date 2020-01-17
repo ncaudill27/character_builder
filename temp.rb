ec96fd655ef095847ebe34c334a79795f4e2b2e8
ncaudill27 <nelson.a.caudill@gmail.com>
2020-01-17 14:09:02 -0500

    Start forming models for Scraper and Klass

--- temp.rb
+++ temp.rb
@@ -2,7 +2,7 @@ require 'nokogiri'
 require 'open-uri'
 require 'pry'
 
-doc = Nokogiri::HTML(open("http://dnd5eapi.co/api/classes/barbarian"))
+#doc = Nokogiri::HTML(open("http://dnd5eapi.co/api/classes/barbarian"))
 
 # Source URL: http://dnd5eapi.co/api/classes/
 # class names
@@ -12,23 +12,36 @@ doc = Nokogiri::HTML(open("http://dnd5eapi.co/api/classes/barbarian"))
 # class urls
 # urls = doc.text.scan(/\/api\/classes\/[a-z]+/i)
 
-# class Klass
-#   @@all = []
-#   attr_accessor :url, :name
-#   def initialize
-#     @@all << self
-#   end
-#   def self.all
-#     @@all
-#   end
-# end
+class Klass
+  @@all = []
 
-# names.each.with_index do |name, index|
-#   c = Klass.new
-#   c.name = name
-#   c.url = urls[index]
+  attr_accessor :url, :name, :hit_die, :skills, :proficiencies
+  
+  def initialize
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+end
 
-# end
+class Scraper
+  
+  def scrape_classes(url)
+    doc = Nokogiri::HTML(open(url))
+    names = doc.text.scan(/name...[a-z]+/i).collect{|klass| klass.match(/[A-Z].+/).to_s}
+    urls = doc.text.scan(/\/api\/classes\/[a-z]+/i)
+    
+    names.each.with_index do |name, index|
+      c = Klass.new
+      c.name = name
+      c.url = urls[index]
+    end
+  end
+end
+
 
 binding.pry
 
