2a15cc859277abdada2e512d2f30f66913327026
ncaudill27 <nelson.a.caudill@gmail.com>
2020-01-18 11:22:30 -0500

    Update characers/edit route for new association

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -25,12 +25,14 @@ class CharactersController < ApplicationController
         @character.klass = Klass.find_by(params[:klass])
         @character.user = current_user
         @character.save
-        
+
         redirect "/characters/#{@character.id}"
     end
 
     get "/characters/:id/edit" do
         @character = Character.find_by(id: params[:id])
+        @races = Race.all
+        @klasses = Klass.all
 
         erb :"/characters/edit"
     end
@@ -38,6 +40,8 @@ class CharactersController < ApplicationController
     patch "/characters/:id" do
         character = Character.find_by(id: params[:id])
         character.update(params[:character])
+        character.klass.update(params[:klass])
+        character.race.update(params[:race])
         
         redirect "/characters/#{character.id}"
     end
