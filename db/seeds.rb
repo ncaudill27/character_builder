0e95d18d366a8236ba72686d21a446f053b19a33
ncaudill27 <nelson.a.caudill@gmail.com>
2020-01-18 20:42:02 -0500

    Create generater for seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,38 +1,24 @@
-require './lib/scraper'
+def random_name
+  names = ["Another Dummy", "Chappie", "The Dude", "Babushka", "Dummy", "El Diablo"]
+  names[rand(names.count)]
+end
 
-c = Character.create(user_id: 4, name: "Another Dummy", hp: 100, str: 12, con: 12, dex: 12, int: 12, wis: 12, cha: 12)
-c.race = Race.all[rand(8)]
-c.klass = Klass.all[rand(11)]
-c = Character.create(user_id: 6, name: "Another Dummy", hp: 100, str: 12, con: 12, dex: 12, int: 12, wis: 12, cha: 12)
-c.race = Race.all[rand(8)]
-c.klass = Klass.all[rand(11)]
-c = Character.create(user_id: 8, name: "Chappie", hp: 100, str: 12, con: 12, dex: 12, int: 12, wis: 12, cha: 12)
-c.race = Race.all[rand(8)]
-c.klass = Klass.all[rand(11)]
-c = Character.create(user_id: 6, name: "The Dude", hp: 100, str: 12, con: 12, dex: 12, int: 12, wis: 12, cha: 12)
-c.race = Race.all[rand(8)]
-c.klass = Klass.all[rand(11)]
-c = Character.create(user_id: 5, name: "Babushka", hp: 100, str: 12, con: 12, dex: 12, int: 12, wis: 12, cha: 12)
-c.race = Race.all[rand(8)]
-c.klass = Klass.all[rand(11)]
-c = Character.create(user_id: 1, name: "Dummy", hp: 100, str: 12, con: 12, dex: 12, int: 12, wis: 12, cha: 12)
-c.race = Race.all[rand(8)]
-c.klass = Klass.all[rand(11)]
-c = Character.create(user_id: 2, name: "Dummy", hp: 100, str: 12, con: 12, dex: 12, int: 12, wis: 12, cha: 12)
-c.race = Race.all[rand(8)]
-c.klass = Klass.all[rand(11)]
-c = Character.create(user_id: 4, name: "Dummy", hp: 100, str: 12, con: 12, dex: 12, int: 12, wis: 12, cha: 12)
-c.race = Race.all[rand(8)]
-c.klass = Klass.all[rand(11)]
-c = Character.create(user_id: 10, name: "Dummy", hp: 100, str: 12, con: 12, dex: 12, int: 12, wis: 12, cha: 12)
-c.race = Race.all[rand(8)]
-c.klass = Klass.all[rand(11)]
-c = Character.create(user_id: 2, name: "Dummy", hp: 100, str: 12, con: 12, dex: 12, int: 12, wis: 12, cha: 12)
-c.race = Race.all[rand(8)]
-c.klass = Klass.all[rand(11)]
-c = Character.create(user_id: 5, name: "Dummy", hp: 100, str: 12, con: 12, dex: 12, int: 12, wis: 12, cha: 12)
-c.race = Race.all[rand(8)]
-c.klass = Klass.all[rand(11)]
-c = Character.create(user_id: 9, name: "El Diablo", hp: 100, str: 12, con: 12, dex: 12, int: 12, wis: 12, cha: 12)
-c.race = Race.all[rand(8)]
-c.klass = Klass.all[rand(11)]
+def random_active_user_id
+  ids = [1, 2, 4, 5, 6, 7, 8, 9, 10, 11]
+  ids[rand(ids.count)]
+end
+
+def generate_characters
+  25.times do
+    c = Character.new(user_id: random_active_user_id, name: random_name, hp: rand(100), str: rand(20), con: rand(20), dex: rand(20), int: rand(20), wis: rand(20), cha: rand(20))
+    c.race = Race.all[rand(Race.all.count)]
+    c.klass = Klass.all[rand(Klass.all.count)]
+    c.save
+  end
+end
+
+generate_characters
+
+# ["Dragonborn", "Dwarf", "Elf", "Gnome", "Half-Elf", "Half-Orc", "Halfling", "Human", "Tiefling"]
+
+# ["Barbarian", "Bard", "Cleric", "Druid", "Fighter", "Monk", "Paladin", "Ranger", "Rogue", "Sorcerer", "Warlock", "Wizard"]
